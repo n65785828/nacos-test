e29008632885e098556f82373df22531521fdf9e
niyihua <747139588@qq.com>
2020-09-10 16:35:14 +0800

    st3

--- src/main/java/cn/yihua/nacostest/controller/TestController.java
+++ src/main/java/cn/yihua/nacostest/controller/TestController.java
@@ -34,20 +34,7 @@ public class TestController {
         return sleepSecends+" second prepare data";
     }
 
-    @GetMapping("/test2")
-    public String test1(){
-        return "test2";
-    }
 
-    @GetMapping("/test3")
-    public String test3(){
-        return "test3";
-    }
-
-    @GetMapping("/test4")
-    public String test4(){
-        return "test4";
-    }
 
 
 }
