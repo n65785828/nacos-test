bac0c24ebda33d34fc3aa5b2eb133ff3ce1a008e
niyihua <747139588@qq.com>
2020-09-10 16:35:01 +0800

    st2

--- src/main/java/cn/yihua/nacostest/controller/TestController.java
+++ src/main/java/cn/yihua/nacostest/controller/TestController.java
@@ -49,13 +49,5 @@ public class TestController {
         return "test4";
     }
 
-    @GetMapping("/test5")
-    public String test5(){
-        return "test5";
-    }
 
-    @GetMapping("/test6")
-    public String test6(){
-        return "test6";
-    }
 }
